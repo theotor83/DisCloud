756c684c918dae47dfcef7ca919626838b54c40f
Théo Torregrossa <152283855+theotor83@users.noreply.github.com>
2025-12-15 16:29:02 +0100

    Add a beta choose provider page (placeholder for now)

--- apps/files/urls.py
+++ apps/files/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
     # URL for the file upload page
     path('upload/', views.upload_file, name='upload'),
 
+    # URL for choosing storage provider type, when creating a new provider
+    path('choose-provider/', views.choose_provider, name='choose_provider'),
+
     # URL for viewing the details of a specific file
     # e.g., /file/1/
     path('file/<int:file_id>/', views.file_detail, name='detail'),
